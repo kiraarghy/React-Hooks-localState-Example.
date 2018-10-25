bd216faf502e0b5847a97bd0d13c3112ddb9b419
Kara Stubbs <karaestubbs@gmail.com>
2018-10-25 22:14:39 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Hooks-localState-Example.
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [word, setWord] = useState(
+    window.localStorage.getItem("word") == null
+      ? ""
+      : window.localStorage.getItem("word")
+  );
+
+  const saveToLocalStorage = word => {
+    window.localStorage.setItem("word", word);
+  };
+
+  function clearLocalStorage() {
+    window.localStorage.clear();
+  }
+
+  function clearWord() {
+    setWord("");
+  }
+
+  function handleNameChange(e) {
+    setWord(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <div className="flex">
+        <form>
+          <label>Input a word:</label>
+          <input
+            className="input flex-child"
+            value={word}
+            onChange={e => handleNameChange(e)}
+          />
+        </form>
+        <button
+          className="flex-child"
+          title="save to localstorage"
+          onClick={() => saveToLocalStorage(word)}
+        >
+          Add word to localStorage
+        </button>
+        <button className="flex-child" onClick={() => clearLocalStorage()}>
+          Clear localStorage
+        </button>
+        <button className="flex-child" onClick={() => clearWord()}>
+          Clear input
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
